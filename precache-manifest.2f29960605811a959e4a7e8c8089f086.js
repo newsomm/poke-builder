6b273e2c48ea9787fa580bc20ebb03aedee2b4cf
Mason Newsom <masonnewsom1@gmail.com>
2020-04-05 22:49:46 -0400

    Updates

---
+++ precache-manifest.2f29960605811a959e4a7e8c8089f086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf0e6f20d82d7618ea23f0b02a89464",
+    "url": "/poke-builder/index.html"
+  },
+  {
+    "revision": "838ea96a003d471ed06b",
+    "url": "/poke-builder/static/css/main.329e0e67.chunk.css"
+  },
+  {
+    "revision": "d37d01247496ef2fbf3f",
+    "url": "/poke-builder/static/js/2.7caba612.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/poke-builder/static/js/2.7caba612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838ea96a003d471ed06b",
+    "url": "/poke-builder/static/js/main.ef1ad66b.chunk.js"
+  },
+  {
+    "revision": "f5668b7dc3e27e73e18d",
+    "url": "/poke-builder/static/js/runtime-main.21f4f9a9.js"
+  }
+]);
